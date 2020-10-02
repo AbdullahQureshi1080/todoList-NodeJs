711c1efbd1fe817b5690a9bcb8ade7052e9ed159
AbdullahQureshi <adnx1080@gmail.com>
2020-10-03 01:00:53 +0500

    DB Implementation - insert and delete

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const express = require("express");
 // Node JS Body Passing Middleware
 const bodyParser = require("body-parser");
 
+// DB Driver
+const mongoose = require("mongoose");
+
 // Import our own date module
-const date = require(__dirname + "/date.js");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+// let items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
 
 // Using EJS Tempelate
@@ -20,24 +23,70 @@ app.set("view engine", "ejs");
 // parses the request, asscessed by req.body
 app.use(bodyParser.urlencoded({ extented: true }));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/todoListDB", {
+  useNewUrlParser: true,
+});
+
+const itemsScheme = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsScheme);
+
+const item1 = new Item({
+  name: "Welcome to your todo List.",
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
+const item3 = new Item({
+  name: "<-- Hit this ro delete an item.",
+});
+
+const defaultItems = [item1, item2, item3];
+
+// delete Route
+
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Success in deleteing the item");
+      res.redirect("/");
+    }
+  });
+});
 
 // Main List Route - Home
 app.get("/", function (req, res) {
-  const day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
+  // const day = date.getDate();
+
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successful added the items to db");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
   console.log(req.body);
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
 });
 
 // Work List Route
